7e94782dd97904e8b49770cdd6e442047a2d031e
Sumairkhanmd <mailstosumair@gmail.com>
2023-08-30 11:06:03 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/nav.js 	new file:   src/images/tech_noir__cyberpunk_time-travel_portal_play_with (1).jpg 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   tailwind.config.js

---
+++ src/components/nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return (
+        <div className="absolute bg-black top-0 left-20 shadow-md bg-opacity-60 h-[80px] flex justify-center w-[900px] rounded-b-lg z-10">
+            hello
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default Nav
